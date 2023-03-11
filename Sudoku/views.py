9837b76081f001af83add0effdc7536381c5b592
Volinger <tisovskyerik@gmail.com>
2023-03-11 14:44:59 +0100

    added documentation to sudoku view

--- Sudoku/views.py
+++ Sudoku/views.py
@@ -10,21 +10,48 @@ from .models import Difficulty, Results
 import json
 
 
-def prepare_sudoku_context(difficulty_setting, size):
+def prepare_sudoku_context(size, difficulty_setting):
+	"""
+	Based on sudoku configuration params (difficulty_setting and size), prepare context containing sudoku data which
+	will be used by UI.
+	:param difficulty_setting:
+	:param size:
+	:return:
+	"""
 	sudoku = SudokuHandler()
 	sudoku.generate(size=size)
-	difficulty = int(size ** 2 * difficulty_setting)
+	difficulty = calculate_difficulty(size=size, difficulty_setting=difficulty_setting)
 	sudoku.prepare_for_solving(difficulty=difficulty)
 	context = {'grid': sudoku.user_grid.tolist(),
 			   'completed_grid': sudoku.completed_grid.tolist()}
 	return context
 
 
+def calculate_difficulty(size, difficulty_setting):
+	"""
+	Sudoku package defines difficulty as number of fields which should be removed from grid. Since we define it as %
+	of the grid which should be removed, conversion is perfomed here.
+	:param size:
+	:param difficulty_setting:
+	:return:
+	"""
+	difficulty = int(size ** 2 * difficulty_setting)
+	return difficulty
+
+
 class SudokuViewSet(viewsets.GenericViewSet):
+	"""
+	ViewSet for displaying sudoku to user as well as related highscores tables.
+	"""
 
 	@action(detail=False, methods=['get'])
 	def sudoku(self, request):
-		size = 9
+		"""
+		Renders sudoku view with default new game settings.
+		:param request:
+		:return:
+		"""
+		size = 9  # Default sudoku grid size
 		difficulties = [value[0] for value in Difficulty.objects.values_list('Option')]
 		difficulty_setting = Difficulty.objects.filter(Option=difficulties[0]).values('FieldsToRemove')[0][
 			'FieldsToRemove']
@@ -34,6 +61,11 @@ class SudokuViewSet(viewsets.GenericViewSet):
 
 	@action(detail=False, methods=['post'])
 	def send_result(self, request):
+		"""
+		Process completed game results from frontend and parse them to database.
+		:param request:
+		:return:
+		"""
 		data = json.loads(request.data['result'])
 		if request.user.is_authenticated:
 			data['UserId'] = request.user
@@ -47,14 +79,26 @@ class SudokuViewSet(viewsets.GenericViewSet):
 
 	@action(detail=False, methods=['get'])
 	def generate_new(self, request):
+		"""
+		Creates new sudoku and sends corresponding data to UI.
+		:param request:
+		:return:
+		"""
 		difficulty_option = request.query_params['Difficulty']
-		difficulty_setting = Difficulty.objects.filter(Option=difficulty_option).values('FieldsToRemove')[0]['FieldsToRemove']
+		difficulty_setting = Difficulty.objects.filter(Option=difficulty_option).values('FieldsToRemove')[0][
+			'FieldsToRemove']
 		size = int(request.query_params['Size'])
 		context = prepare_sudoku_context(difficulty_setting=difficulty_setting, size=size)
 		return Response(data=context)
 
 	@action(detail=False, methods=['get'], url_path=r'leaderboards-view/(?P<difficulty>[\w-]+)')
 	def leaderboards_view(self, request, difficulty=None):
+		"""
+		Renders UI which displays best scores.
+		:param request:
+		:param difficulty:
+		:return:
+		"""
 		difficulties = [value[0] for value in Difficulty.objects.values_list('Option')]
 		leaderboards = [values for values in
 						ResultSerializer(Results.objects.filter(Difficulty__Option=difficulty).order_by(
