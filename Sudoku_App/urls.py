474cddb78c430987fb2aa16057dd0ce962810658
Volinger <tisovskyerik@gmail.com>
2023-03-12 13:39:47 +0100

    static files

--- Sudoku_App/urls.py
+++ Sudoku_App/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name="home"),
@@ -23,3 +25,5 @@ urlpatterns = [
     path('sudoku/', include('Sudoku.urls')),
     path('auth/', include('Login.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
